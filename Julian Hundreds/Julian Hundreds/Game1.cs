e9bff5d25437959e91c9be2544c1a2690692dfe1
Julian <juicystarboy@gmail.com>
2018-01-28 11:20:37 -0800

    Added all files

---
+++ Julian Hundreds/Julian Hundreds/Game1.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Julian_Hundreds
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        Texture2D circle;
+        Texture2D pixel;
+        Texture2D strike;
+        SpriteFont arial;
+        SpriteFont big;
+        List<Ball> balls;
+        List<int> xlocs;
+        List<int> ylocs;
+        List<int> xspeeds;
+        List<int> yspeeds;
+        List<Ball> strikeballs;
+        int nextxloc;
+        int nextyloc;
+        Random rand;
+        bool lost;
+        int score = 0;
+        int strikes = 0;
+        int speed = 2;
+        int numofballs = 30;
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.ApplyChanges();
+            IsMouseVisible = true;
+            Mouse.SetPosition(-100, -100);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            balls = new List<Ball>();
+            xlocs = new List<int>();
+            ylocs = new List<int>();
+            xspeeds = new List<int>();
+            yspeeds = new List<int>();
+            strikeballs = new List<Ball>();
+            rand = new Random();
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            circle = Content.Load<Texture2D>("clearcircle");
+            pixel = Content.Load<Texture2D>("pixel");
+            strike = Content.Load<Texture2D>("strike");
+            arial = Content.Load<SpriteFont>("File");
+            big = Content.Load<SpriteFont>("big");
+            for (int i = 0; i < numofballs; i++)
+            {
+                nextxloc = rand.Next(0, (graphics.GraphicsDevice.Viewport.Width - 50));
+                nextyloc = rand.Next(0, (graphics.GraphicsDevice.Viewport.Height - 50));
+                xlocs.Add(nextxloc);
+                ylocs.Add(nextyloc);
+                xspeeds.Add(rand.Next(-1, 1) == 0 ? speed : -speed);
+                yspeeds.Add(rand.Next(-1, 1) == 0 ? speed : -speed);
+                balls.Add(new Ball(xlocs[i], ylocs[i], xspeeds[i], yspeeds[i], circle, graphics.GraphicsDevice, balls.Count - 1));
+            }
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (lost || score >= 100)
+            {
+                if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                {
+                    xspeeds.Clear();
+                    yspeeds.Clear();
+                    xlocs.Clear();
+                    ylocs.Clear();
+                    balls.Clear();
+                    strikes = 0;
+                    for (int i = 0; i < numofballs; i++)
+                    {
+                        Mouse.SetPosition(-100, -100);
+                        nextxloc = rand.Next(0, (graphics.GraphicsDevice.Viewport.Width - 50));
+                        nextyloc = rand.Next(0, (graphics.GraphicsDevice.Viewport.Height - 50));
+                        xlocs.Add(nextxloc);
+                        ylocs.Add(nextyloc);
+                        xspeeds.Add(rand.Next(-1, 1) == 0 ? speed : -speed);
+                        yspeeds.Add(rand.Next(-1, 1) == 0 ? speed : -speed);
+                        balls.Add(new Ball(xlocs[i], ylocs[i], xspeeds[i], yspeeds[i], circle, graphics.GraphicsDevice, balls.Count - 1));
+                    }
+                    lost = false;
+                    foreach (Ball b in balls)
+                    {
+                        b.lost = false;
+                    }
+                }
+                return;
+            }
+            // TODO: Add your update logic here
+            foreach (Ball b in balls)
+            {
+                b.Move(balls);
+                if (b.lost)
+                {
+                    strikeballs.Add(b);
+                    strikes++;
+                    if (strikes >= 6)
+                    {
+                        lost = true;
+                    }
+                }
+            }
+            for (int i = 0; i < strikeballs.Count; i++)
+            {
+                balls.Remove(strikeballs[i]);
+            }
+            strikeballs.Clear();
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            if (lost)
+            {
+                GraphicsDevice.Clear(Color.Red);
+            }
+            else if (score >= 100)
+            {
+                GraphicsDevice.Clear(Color.Green);
+            }
+            else
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+            }
+            spriteBatch.Begin();
+            score = 0;
+            // TODO: Add your drawing code here
+            foreach (Ball b in balls)
+            {
+                //spriteBatch.Draw(pixel, b.X, Color.White);
+                //spriteBatch.Draw(pixel, b.Y, Color.White);
+                b.Draw(spriteBatch, arial);
+                spriteBatch.DrawString(arial, ((b.colsize - 50) / 10).ToString(), new Vector2(b.Y.X - arial.MeasureString(((b.colsize - 50) / 10).ToString()).X / 2, b.X.Y - arial.MeasureString(((b.colsize - 50) / 10).ToString()).Y / 2), Color.White);
+                //spriteBatch.Draw(pixel, new Rectangle(Mouse.GetState().Position.X, Mouse.GetState().Position.Y, 1,1), Color.White);
+                score += ((b.colsize - 50) / 10);
+            }
+            spriteBatch.DrawString(big, score.ToString(), new Vector2(0, 0), Color.White);
+            if (strikes >= 2)
+            {
+                spriteBatch.Draw(strike, new Vector2(GraphicsDevice.Viewport.Width - 3 * strike.Width, 0), Color.White);
+                if (strikes >= 4)
+                {
+                    spriteBatch.Draw(strike, new Vector2(GraphicsDevice.Viewport.Width - 2 * strike.Width, 0), Color.White);
+                    if (strikes >= 6)
+                    {
+                        spriteBatch.Draw(strike, new Vector2(GraphicsDevice.Viewport.Width - strike.Width, 0), Color.White);
+                    }
+                }
+            }
+            if (lost)
+            {
+                spriteBatch.DrawString(big, "You Lost", new Vector2(GraphicsDevice.Viewport.Width / 2 - big.MeasureString("You Lost").X / 2, 0), Color.White);
+            }
+            if (score >= 100)
+            {
+                spriteBatch.DrawString(big, "You Win", new Vector2(GraphicsDevice.Viewport.Width / 2 - big.MeasureString("You Win").X / 2, 0), Color.White);
+            }
+            base.Draw(gameTime);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Julian Hundreds/Julian Hundreds/ball.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Julian_Hundreds
+{
+    class Ball
+    {
+        public int x;
+        public int y;
+        public int ix;
+        public int iy;
+        public int identifier;
+        public int colsize = 50;
+        public int size = 50;
+        public bool lost;
+        public Rectangle Hitbox;
+        public Rectangle Y;
+        public Rectangle X;
+        List<Ball> balls;
+        Texture2D circle;
+        GraphicsDevice graphicsdevice;
+        float scale = 1f;
+        public bool isGrowing = false;
+
+        public Ball(int x, int y, int ix, int iy, Texture2D circle, GraphicsDevice graphicsdevice, int identifier)
+        {
+            this.identifier = identifier;
+            this.x = x;
+            this.y = y;
+            this.ix = ix;
+            this.iy = iy;
+            this.circle = circle;
+            this.graphicsdevice = graphicsdevice;
+            circle = new Texture2D(graphicsdevice, colsize, colsize);
+            Hitbox = new Rectangle(x, y, colsize, colsize);
+        }
+
+        public void Move(List<Ball> balls)
+        {
+            isGrowing = false;
+            this.balls = balls;
+            int cursorxdis = Math.Abs(Mouse.GetState().Position.X - Y.X);
+            int cursorydis = Math.Abs(Mouse.GetState().Position.Y - X.Y);
+            int cursordis = (int)Math.Sqrt((cursorxdis * cursorxdis) + (cursorydis * cursorydis));
+
+            Y = new Rectangle(x + ((int)(size * scale) / 2), y, 1, (int)(size * scale));
+            X = new Rectangle(x, y + ((int)(size * scale) / 2), (int)(size * scale), 1);
+            if (x + colsize >= graphicsdevice.Viewport.Width && ix > 0)
+            {
+                ix = -Math.Abs(ix);
+            }
+            else if (x <= 0 && ix < 0)
+            {
+                ix = Math.Abs(ix);
+            }
+            if (y + colsize >= graphicsdevice.Viewport.Height && iy > 0)
+            {
+                iy = -Math.Abs(iy);
+            }
+            if (y <= 0 && iy < 0)
+            {
+                iy = Math.Abs(iy);
+            }
+            if (cursordis < colsize / 2)
+            {
+                scale += 0.05f;
+                colsize = (int)(size * scale);
+                x -= (int)(.025 * size);
+                y -= (int)(.025 * size);
+                isGrowing = true;
+            }
+            foreach (Ball b in balls)
+            {
+                bool isColliding = false;
+                if (b != this)
+                {
+                    int xdis = (b.Y.X - Y.X);
+                    int ydis = (b.X.Y - X.Y);
+                    int distance = (int)Math.Sqrt((xdis * xdis) + (ydis * ydis));
+                    int minDis = colsize / 2 + b.colsize / 2;
+                    if (b.identifier != identifier)
+                    {
+                        if (b.Hitbox.Intersects(Hitbox))
+                        {
+                            if (xdis > 0 && ydis > 0 && distance <= (colsize / 2 + b.colsize / 2))
+                            {
+                                ix = -Math.Abs(ix);
+                                iy = -Math.Abs(iy);
+                                b.ix = Math.Abs(b.ix);
+                                b.iy = Math.Abs(b.iy);
+                                isColliding = true;
+                            }
+                            if (xdis < 0 && ydis < 0 && distance <= (colsize / 2 + b.colsize / 2))
+                            {
+                                ix = Math.Abs(ix);
+                                iy = Math.Abs(iy);
+                                b.ix = -Math.Abs(b.ix);
+                                b.iy = -Math.Abs(b.iy);
+                                isColliding = true;
+                            }
+                            if (xdis < 0 && ydis > 0 && distance <= (colsize / 2 + b.colsize / 2))
+                            {
+                                ix = Math.Abs(ix);
+                                iy = -Math.Abs(iy);
+                                b.ix = -Math.Abs(b.ix);
+                                b.iy = Math.Abs(b.iy);
+                                isColliding = true;
+                            }
+                            if (xdis > 0 && ydis < 0 && distance <= (colsize / 2 + b.colsize / 2))
+                            {
+                                ix = -Math.Abs(ix);
+                                iy = Math.Abs(iy);
+                                b.ix = Math.Abs(b.ix);
+                                b.iy = -Math.Abs(b.iy);
+                                isColliding = true;
+                            }
+                        }
+                    }
+                    cursorxdis = Math.Abs(Mouse.GetState().Position.X - Y.X);
+                    cursorydis = Math.Abs(Mouse.GetState().Position.Y - X.Y);
+                    cursordis = (int)Math.Sqrt((cursorxdis * cursorxdis) + (cursorydis * cursorydis));
+                    //if (cursordis < colsize / 2)
+                    //{
+                    //xdis = (b.Y.X - Y.X);
+                    //ydis = (b.X.Y - X.Y);
+                    //distance = (int)Math.Sqrt((xdis * xdis) + (ydis * ydis));
+                    //minDis = colsize / 2 + b.colsize / 2;
+                    if (isGrowing && isColliding || b.isGrowing && isColliding /*distance < minDis*/)
+                    {
+                        lost = true;
+                        b.lost = true;
+                    }
+                    //}
+                }
+
+            }
+            x += ix;
+            y += iy;
+            Hitbox = new Rectangle(x, y, (int)(size * scale), (int)(size * scale));
+        }
+
+        public void Draw(SpriteBatch spritebatch, SpriteFont arial)
+        {
+            //spritebatch.Draw(circle, new Vector2(Y.Y, X.X), null, Color.White, 0f, new Vector2(Y.X, X.Y), scale, SpriteEffects.None, 0f);
+            spritebatch.Draw(circle, Hitbox, Color.White);
+            //spritebatch.DrawString(arial, ix.ToString(), new Vector2((Y.X-arial.MeasureString(ix.ToString()).X/2), X.Y-14), Color.White);
+            //spritebatch.DrawString(arial, iy.ToString(), new Vector2((Y.X - arial.MeasureString(ix.ToString()).X / 2), X.Y + 2), Color.White);
+        }
+    }
+}
